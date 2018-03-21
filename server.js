f7376ed717958841a026673a18588780fae49680
Erin McMackin <mcmackinerin@gmail.com>
2018-03-21 15:58:26 -0400

    index created and rendered

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ const db = mongoose.connection;
 
 mongoose.connect(mongoURI);
 
+// checking for connection to mongo
 db.on('connected', ()=>{
   console.log('mongo connected: ', mongoURI);
-})
+});
+
+
+// ROUTES
+app.get('/', (req, res)=>{
+  res.render('index.ejs');
+});
+
 
 app.listen(3000, ()=>{
   console.log('I\'m listening...');
