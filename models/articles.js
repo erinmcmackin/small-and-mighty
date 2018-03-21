501749f55695a812029ab4655898e34b990369fb
Erin McMackin <mcmackinerin@gmail.com>
2018-03-21 15:15:20 -0400

    article model created

---
+++ models/articles.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  body: {type: String, required: true},
+  summary: {type: String, required: true},
+  author: {type: String, required: true},
+  img: {type: String, required: true}
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
