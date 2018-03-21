e69ee36da0a26344cd4d92c76b869d9fa15166b1
Erin McMackin <mcmackinerin@gmail.com>
2018-03-21 15:44:26 -0400

    connected mongoose

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const mongoURI = 'mongodb://localhost:27017/logs';
+const db = mongoose.connection;
 
+mongoose.connect(mongoURI);
+
+db.on('connected', ()=>{
+  console.log('mongo connected: ', mongoURI);
+})
 
 app.listen(3000, ()=>{
   console.log('I\'m listening...');
